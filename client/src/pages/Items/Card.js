a7404d5f8c01fed2e118e2b560d440b9947d4e0b
Alex - Home <alex.home@Administrators-MacBook-Pro-2.local>
2019-06-01 16:54:31 -0700

    Adding style to the items page, adding gravatar

--- client/src/pages/Items/Card.js
+++ client/src/pages/Items/Card.js
@@ -1,70 +1,98 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
-
+import React from "react";
+import PropTypes, { element } from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Gravatar from "react-gravatar";
 
 const styles = {
   card: {
-   // maxWidth: 345,
-   marginLeft: '10%',
-   marginTop: 20,
-    
+    maxHeight: 500,
+    marginTop: 0
+  },
+  gravatar: {
+    borderRadius: "50%"
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%" // 16:9
   },
-  user_info_container :{
-    display: 'flex',
-    marginBottom: '50px'
+  user_info_container: {
+    display: "flex",
+    marginBottom: "50px"
   },
-  user_info_right :{
-    marginLeft: '10px',
+  user_info_right: {
+    marginLeft: "10px"
   },
-  user_span :{
-    margin: '0px',
+  user_span: {
+    margin: "0px"
+  },
+  spanTags: {
+    display: "inline",
+    textTransform: "capitalize",
+    marginRight: 5,
+    color: "grey"
   }
 };
 
+const test1 = id => {
+  window.location.pathname = "profile";
 
+  window.item_id = id;
+};
+export const test2 = 0;
 
-const test1 = (id) =>{
- window.location.pathname = 'profile';
-  console.log(id)
-  window.item_id  = id;
-}
-export const test2 = 0
+function ItemCard(props) {
+  const {
+    classes,
+    title,
+    description,
+    imageUrl,
+    item_owner,
+    item_id,
+    tags
+  } = props;
+  console.log(tags);
+  let fetchImg =
+    imageUrl == null
+      ? "http://via.placeholder.com/350x250?text=Please%20select%20an%20image"
+      : imageUrl;
 
-
-
-function SimpleMediaCard(props) {
-  const { classes, title ,description , imageUrl , item_owner , item_id} = props;
-  
   return (
     <div>
       <Card className={classes.card}>
         <CardMedia
           className={classes.media}
-          image= {imageUrl}
-          title= {description}
+          image={fetchImg}
+          title={description}
         />
         <CardContent>
           <div className={classes.user_info_container}>
             <div>
-              <ImageAvatars />
+              <Gravatar
+                email={item_owner + "@red.com/d=retro"}
+                className={classes.gravatar}
+              />
             </div>
             <div className={classes.user_info_right}>
-              <Typography gutterBottom fontSize={12} component="span" className={classes.user_span}>
+              <Typography
+                gutterBottom
+                fontSize={12}
+                component="span"
+                className={classes.user_span}
+              >
                 {item_owner}
               </Typography>
-              <Typography gutterBottom fontSize={2} component="span" className={classes.user_span}>
+              <Typography
+                gutterBottom
+                fontSize={2}
+                component="span"
+                className={classes.user_span}
+              >
                 Hour TODO
               </Typography>
             </div>
@@ -72,13 +100,33 @@ function SimpleMediaCard(props) {
           <Typography gutterBottom variant="headline" component="h2">
             {title}
           </Typography>
-          <Typography component="p">
-            {description}
-          </Typography>
+
+          {tags.map(element => {
+            return (
+              <Typography
+                gutterBottom
+                component="span"
+                className={classes.spanTags}
+              >
+                {element.title}
+              </Typography>
+            );
+          })}
+          <Typography
+            gutterBottom
+            component="span"
+            className={classes.spanTags}
+          />
+          <Typography component="p">{description}</Typography>
         </CardContent>
         <CardActions>
-          <Button variant="outlined" className={classes.button} onClick={()=>test1(item_id)} id={item_id}>
-          BORROW
+          <Button
+            variant="outlined"
+            className={classes.button}
+            onClick={() => test1(item_id)}
+            id={item_id}
+          >
+            BORROW
           </Button>
         </CardActions>
       </Card>
@@ -86,24 +134,8 @@ function SimpleMediaCard(props) {
   );
 }
 
-SimpleMediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
+ItemCard.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-
-
-function ImageAvatars(props) {
-  const { classes } = props;
-  return (
-    <div>
-      <Avatar alt="Logo" src='https://cdn.pixabay.com/photo/2016/04/15/18/05/computer-1331579_1280.png' />
-    </div>
-  );
-}
-
-ImageAvatars.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-
-export default withStyles(styles)(SimpleMediaCard);
+export default withStyles(styles)(ItemCard);
--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,42 +1,89 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
-const divStyle = {
-  minWidth: '100%',
-  background: 'black'
-  
-};
+const styles = theme => ({
+  root: {
+    background: "black"
+  },
+  gridContainer: {
+    paddingTop: 50,
+    width: "90%",
+    margin: "auto",
+    flexGrow: 1
+  },
+  paper: {
+    height: 140,
+    width: 100
+  },
+  control: {
+    padding: theme.spacing.unit * 2
+  }
+});
 
-const ulStyle = {
-  margin: 'auto',
-  marginTop:'5%'
-};
+class Items extends React.Component {
+  state = {
+    spacing: "16"
+  };
 
-const liStyle = {
-  color: 'white',
-  display: 'inline-block',
-  minWidth: '45%',
-};
+  handleChange = key => (event, value) => {
+    this.setState({
+      [key]: value
+    });
+  };
+  render() {
+    const { classes, items } = this.props;
+    const { spacing } = this.state;
 
-const Items = ({ classes, items }) => {
-
-  
-  return (
-    <div style={divStyle}>   
-      <ul style={ulStyle}>
-        {items.map(element=> 
-        <li style={liStyle} key={element.id}>
-          <Card 
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={40} className={classes.gridContainer}>
+          {items.map(element => (
+            <Grid item xs={12} sm={6} md={4}>
+              <Card
                 item_owner={element.itemowner.fullname}
                 title={element.title}
                 description={element.description}
                 imageUrl={element.imageurl}
-                item_id={element.id} />
-        </li>)}
-      </ul>
-    </div>
-  );
+                tags={element.tags}
+                item_id={element.id}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    );
+  }
+}
+
+Items.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default Items;
+export default withStyles(styles)(Items);
 
+// <div className={classes.root}>
+//   <Grid container spacing={24}>
+//     <Grid item xs={24} sm={12} md={8}>
+//       <Paper className={classes.paper}>xs=6 sm=3</Paper>
+//     </Grid>
+//   </Grid>
+// </div>;
+
+// <div style={divStyle}>
+//   <ul style={ulStyle}>
+//     {items.map(element => (
+//       <li style={liStyle} key={element.id}>
+//         <Card
+//           item_owner={element.itemowner.fullname}
+//           title={element.title}
+//           description={element.description}
+//           imageUrl={element.imageurl}
+//           item_id={element.id}
+//         />
+//       </li>
+//     ))}
+//   </ul>
+// </div>;
